81067e91aa700d07be2aa4002645f258ee2aaf72
remeh <remeh@remeh.fr>
2015-08-25 22:21:24 +0200

    general: logger helpers.

---
+++ logger/logger.go
@@ -0,0 +1,23 @@
+// Parking Backend - Runtime
+//
+// Logger helpers.
+//
+// 2015
+
+package runtime
+
+import (
+	"log"
+)
+
+func Error(data ...interface{}) {
+	log.Println("Error:", data)
+}
+
+func Warning(data ...interface{}) {
+	log.Println("Warning:", data)
+}
+
+func Info(data ...interface{}) {
+	log.Println("Info:", data)
+}
