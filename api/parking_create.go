76ad11cd31bdf0827b275791605f2809bfc37b2e
yen0x <gege.dert@gmail.com>
2015-09-01 11:23:39 +0200

    le commit de Clémentine

---
+++ api/parking_create.go
@@ -0,0 +1,38 @@
+package api
+
+import (
+	"bitbucket.org/remeh/parking/runtime"
+	"bitbucket.org/remeh/parking/service"
+	"io/ioutil"
+	"net/http"
+)
+
+type CreateParking struct {
+	Runtime *runtime.Runtime
+}
+
+type CreateParkingBody struct {
+	Address     string `json"address"`
+	Description string `json"description"`
+	Price       int    `json"price"`
+}
+
+func (c CreateParking) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	data, err := ioutil.ReadAll(r.Body)
+
+	if err != nil {
+		Error(err)
+		w.WriteHeader(500)
+		return
+	}
+	body := CreateParkingBody{}	
+	json.Unmarshal(data, &body)
+	uuid, err := service.CreateParking(c.Runtime, body.Address, body.Description, body.Price)
+	if err != nil {
+		Error(err)
+		w.WriteHeader(500)
+		return
+	}
+
+	w.Write(uuid)
+}
---
+++ service/parking.go
@@ -0,0 +1,40 @@
+// Parking Backend - Service
+//
+// All methods around parking features
+//
+// 2015
+
+package service
+
+import (
+	"bitbucket.org/remeh/parking/runtime"
+	"bitbucket.org/remeh/parking/model"
+	"github.com/pborman/uuid"
+	"time"
+)
+
+func CreateParking(rt *runtime.Runtime, address, description, price string) (uuid.UUID, error) {
+	if rt == nil {
+		return []byte{}, nil
+	}
+
+	pDAO := rt.Storage.ParkingDAO
+	now := time.Now()
+	uid := uuid.Parse(uuid.New())
+
+    parking := model.Parking{
+		Uid:          uid,
+		UserId:       uuid.Parse("1"),
+		Description:  description,
+		Address:      address,
+		Latitude:     0.0,
+		Longitude:    0.0,
+		DailyPrice:   price,
+		CreationTime: now,
+		LastUpdate:   now,
+	}
+
+	_, err := pDAO.Insert(parking)
+	return uid, err
+	
+}
